85ce9c0757703fe29e48758e0d6bd6e696173433
Jamshidbek Akhlidinov <jamshidbekakhlidinov>
2024-11-06 11:41:56 +0500

    update value

--- array/88_Merge_Sorted_Array.php
+++ array/88_Merge_Sorted_Array.php
@@ -20,6 +20,6 @@ class Solution {
 }
 
 $obj = new Solution();
-$nums1 = [0]; $m = 0; $nums2 = [1]; $n = 1;
+$nums1 = [1,2,3,0,0,0]; $m = 3; $nums2 = [2,5,6]; $n = 3;
 $obj->merge($nums1,$m,$nums2,$n);
 print_r($nums1);
